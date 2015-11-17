b0046b4043013b8aa10432c2e2c91df95de91ded
turboflanagan <turboflanagan@gmail.com>
2015-11-17 11:31:40 -0500

    added a dependency

--- controller.js
+++ controller.js
@@ -1,9 +1,26 @@
-var countryApp = angular.module('countryApp', []);
+var countryApp = angular.module('countryApp', ['ngRoute']);
+
+countryApp.config(function($routeProvider){
+	$routeProvider.when('/',{
+		template: '<ul><li ng-repeat="country in countries">{{country.name}}</li></ul>',
+		controller: 'countryCntrl'
+	}).
+	when('/:countryName',{
+		template: '<h1>Country detail will go here.</h1>',
+		controller: 'countryDetailCntrl'
+	}).
+	otherwise({
+		redirectTo: '/'
+	});
+});
 
 countryApp.controller('countryCntrl', function($scope, $http){
 
 	$http.get('countries.json').success(function(countryData){
 		$scope.countries = countryData;
-		console.log($scope.countries);
 	});
 }); 
+
+countryApp.controller('countryDetailCntrl', function($scope, $routeParams){
+	console.log($routeParams);
+});
